20713946d7e0bca174aca2e5e869118e8af0aa4b
root <root@LAPTOP-QL00RMIT.localdomain>
2019-01-25 17:18:51 -0600

    Created the kb_assert and kb_ask functions

--- student_code.py
+++ student_code.py
@@ -23,8 +23,21 @@ class KnowledgeBase(object):
         Args:
             fact (Fact or Rule): Fact or Rule we're asserting in the format produced by read.py
         """
-        print("Asserting {!r}".format(fact))
-        
+        if factq(fact.name):                #Check to make sure it is a fact
+            for facct in self.facts:        #Loop through all facts in database to see if it produces a match or in other terms is already in the database
+                matchfound = match(fact.statement, facct.statement, bindings=None)
+                if matchfound != False:     #If it finds a match, just return nothing since the fact is already in the database
+                    return None
+            self.facts.append(fact)         #Else, add the fact to the database
+            print("Asserting {!r}".format(fact))
+        elif isinstance(item, Rule):        #Same as above but with rules
+            for rulee in self.rules:
+                matchfound = match(fact.statement, rulee.statement, bindings=None)
+                if matchfound != False:
+                    return None
+            self.rules.append(fact)
+
+
     def kb_ask(self, fact):
         """Ask if a fact is in the KB
 
@@ -34,4 +47,11 @@ class KnowledgeBase(object):
         Returns:
             ListOfBindings|False - ListOfBindings if result found, False otherwise
         """
-        print("Asking {!r}".format(fact))
+
+        bindlist = ListOfBindings()         #Create an empty list of bindings that will be returned
+        for facct in self.facts:            #loop through all facts in the database
+
+            matchbindings = match(facct.statement, fact.statement, bindings=None)   #If a match is found,
+            if matchbindings != False:
+                bindlist.add_bindings(matchbindings, [facct,fact])                  #Add it to the list of bindings
+        return bindlist                                                             #Return the list of bindings or an empty list
